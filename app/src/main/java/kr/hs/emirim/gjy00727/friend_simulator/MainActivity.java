10ecd5bbf0ea80bea2c41518506e8c86d20cf200
KimJiyeon <gjy00727@e-mirim.hs.kr>
2017-04-06 20:54:22 +0900

    #10 로그 남기기

--- app/src/main/java/kr/hs/emirim/gjy00727/friend_simulator/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/gjy00727/friend_simulator/MainActivity.java
@@ -20,20 +20,28 @@ public class MainActivity extends AppCompatActivity {
     void onClick(View view){
         switch (view.getId()){
             case R.id.button1:
+                Log.d(TAG, "버튼1 클릭");
                 Toast.makeText(this, "졸려", Toast.LENGTH_SHORT);
                 break;
             case R.id.button2:
+                Log.d(TAG, "버튼2 클릭");
                 Toast.makeText(this, "효린이 메렁", Toast.LENGTH_SHORT);
                 break;
             case R.id.button3:
+                Log.d(TAG, "버튼3 클릭");
                 Toast.makeText(this, "효린이 바부", Toast.LENGTH_SHORT);
                 break;
             case R.id.button4:
+                Log.d(TAG, "버튼4 클릭");
                 Toast.makeText(this, "효린이 자바 10번 쓰기", Toast.LENGTH_SHORT);
                 break;
             case R.id.button5:
+                Log.e(TAG, "버튼5 클릭");
                 Toast.makeText(this, "효린이 멍청이", Toast.LENGTH_SHORT);
                 break;
+            default:
+                Log.e(TAG, "모르는 녀석 클릭");
+                Toast.makeText(this, "뭐라하는지 모르겠어용", Toast.LENGTH_SHORT);
         }
     }
 }
